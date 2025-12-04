2437825f6d22db2098016ecd20a2cb5067d3929c
Cezar98 <33655735+Cezar98@users.noreply.github.com>
2025-12-04 15:04:41 +0200

    Implement continuous screenshot capture with FPS display
    Capture screenshots continuously for 5 seconds and display FPS.

--- real_time/capture.py
+++ real_time/capture.py
@@ -1,5 +1,20 @@
 from mss import mss
+import time
 
-# The simplest use, save a screenshot of the 1st monitor
+t0 = time.perf_counter()
+
+t = t0
+monitor = {"top": 160, "left": 160, "width": 160, "height": 135}
 with mss() as sct:
-    sct.shot()
+    while t - t0 <= 5:
+
+
+
+        # Grab the data
+        sct_img = sct.grab(monitor)
+        last  = t
+
+        sct.grab(monitor)
+        t  = time.perf_counter()
+        fps = 1 / (t - last)
+        print("FPS: ", fps)
