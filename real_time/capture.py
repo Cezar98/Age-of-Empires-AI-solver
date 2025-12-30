b5bbd79f236cdb22d97be1fca109956bc6f99b02
Cezar98 <33655735+Cezar98@users.noreply.github.com>
2025-12-30 15:45:12 +0200

    Refactor capture logic into capture_frame function
    Refactored the capture logic into a function and added FPS calculation.

--- real_time/capture.py
+++ real_time/capture.py
@@ -1,20 +1,31 @@
 from mss import mss
 import time
-
-t0 = time.perf_counter()
-
-t = t0
-monitor = {"top": 160, "left": 160, "width": 160, "height": 135}
-with mss() as sct:
-    while t - t0 <= 5:
+from windowing import get_client_rect_screen,find_game_window
+import numpy as np
 
 
+def capture_frame():
+    t0 = time.perf_counter()
+    t = t0
+    screen = get_client_rect_screen(find_game_window())
+    monitor = {"top": screen[1], "left": screen[0], "width": screen[2], "height": screen[3]}
+    print(monitor)
+    with mss() as sct:
+        while True:
+            # Grab the data
+            sct_img = sct.grab(monitor)
+            last = t
 
-        # Grab the data
-        sct_img = sct.grab(monitor)
-        last  = t
 
-        sct.grab(monitor)
-        t  = time.perf_counter()
-        fps = 1 / (t - last)
-        print("FPS: ", fps)
+            t = time.perf_counter()
+            fps = 1 / (t - last)
+            print("FPS: ", fps)
+            frame = np.asarray(sct_img)[:, :, :3]  # BGR order
+            frame = frame[:, :, ::-1]
+            yield (t,frame)
+
+
+if __name__ == "__main__":
+    time.sleep(3)
+    frame = capture_frame()
+    print(next(frame)[0])
